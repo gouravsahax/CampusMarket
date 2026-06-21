dfa5033fd364810830a5f520cda13a8ad581d36f
Gouravsaha12 <gouravsaha123098@gmail.com>
2026-06-21 21:10:27 +0530

    navbar ui added

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation';
+
+const Navbar = () => {
+  const path : string = usePathname();
+
+  return (
+    <div className='flex justify-between items-center px-4 md:px-8 border-b-1 border-zinc-900'>
+      <div>CampusMarket</div>
+      <div className='flex gap-6'>
+        <Link href='/' className={`py-2 + ${path === '/' ? 'border-b-2 border-white' : ''}`} >
+            home
+        </Link>
+        <Link href='/' className={`py-2 + ${path === '/requests' ? 'border-b-2 border-white' : ''}`}>
+            requests
+        </Link>
+        <Link href='/' className={`py-2 + ${path === '/profile' ? 'border-b-2 border-white' : ''}`}>
+            profile
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
